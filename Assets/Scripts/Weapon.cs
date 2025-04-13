874fb220f1ceee1888489e757bedd54d546319b7
Behzad <moloudibehzad@gmail.com>
2025-04-13 14:03:57 +0200

    Add shooting cooldown system based on fire rate from WeaponData

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,42 +1,20 @@
-using StarterAssets;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] WeaponSO weaponSO;
-    [SerializeField] GameObject hitVFXPrefab;
-    [SerializeField] Animator animator;
-    [SerializeField] int damageAmount = 1;
+
     [SerializeField] ParticleSystem muzzleFlash;
 
-    StarterAssetsInputs starterAssetsInputs;
-
-    const string SHOOT_STRING = "Shoot";
-
-    void Awake()
+    public void Shoot(WeaponSO weaponSO)
     {
-        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
-    }
-
-    void Update()
-    {
-        HandleShoot();
-    }
-
-    private void HandleShoot()
-    {
-        if (!starterAssetsInputs.shoot) return;
-
-        muzzleFlash.Play();
-        animator.Play(SHOOT_STRING, 0, 0f);
-        starterAssetsInputs.ShootInput(false);
-
         RaycastHit hit;
+        muzzleFlash.Play();
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
-            Instantiate(hitVFXPrefab, hit.point, quaternion.identity);
+
+            Instantiate(weaponSO.HitVFXPrefab, hit.point, Quaternion.identity);
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(weaponSO.Damage);
         }
