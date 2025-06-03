2bc9e54e7073f2f9b407035322715ef148984840
Behzad <moloudibehzad@gmail.com>
2025-06-03 09:14:22 +0200

    Use Physics.OverlapSphere to apply explosion damage to nearby player and enemies

---
+++ Assets/Scripts/Enemies/Robot.cs
@@ -0,0 +1,37 @@
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Robot : MonoBehaviour
+{
+    FirstPersonController player;
+    NavMeshAgent agent;
+
+    const string PLAYER_STRING = "Player";
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        if (!player) return;
+        player = FindFirstObjectByType<FirstPersonController>();
+    }
+
+
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(PLAYER_STRING))
+        {
+            EnemyHealth enemyHealth = GetComponent<EnemyHealth>();
+            enemyHealth.SelfDestruct();
+        }
+    }
+}
